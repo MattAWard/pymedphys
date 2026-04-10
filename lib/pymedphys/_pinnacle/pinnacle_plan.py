e8fbfe9254791a7fd26a0f9a73724cd1afdbb35d
Matt Ward <matthew.ward@uhs.nhs.uk>
2026-04-10 16:46:39 +0100

    comment out active trial setter

--- lib/pymedphys/_pinnacle/pinnacle_plan.py
+++ lib/pymedphys/_pinnacle/pinnacle_plan.py
@@ -179,7 +179,6 @@ class PinnaclePlan:
                 self._trial_info = self._trials[0]
 
             self.logger.debug("Number of trials read: %s", len(self._trials))
-            self.logger.debug("Active Trial: %s", self._trial_info["Name"])
 
         return self._trials
 
@@ -193,16 +192,17 @@ class PinnaclePlan:
 
         return self._trial_info
 
-    @active_trial.setter
-    def active_trial(self, trial_name):
-        if isinstance(trial_name, str):
-            for trial in self.trials:
-                if trial["Name"] == trial_name:
-                    self._trial_info = trial
-                    self.logger.info("Active Trial set: %s", trial_name)
-                    return
 
-        raise KeyError
+    #@active_trial.setter
+    #def active_trial(self, trial_name):
+    #    if isinstance(trial_name, str):
+    #        for trial in self.trials:
+    #            if trial["Name"] == trial_name:
+    #                self._trial_info = trial
+    #                self.logger.info("Active Trial set: %s", trial_name)
+    #                return
+
+    #    raise KeyError
 
     @property
     def plan_info(self):
